274b7367b9979c4e7f5e0f84a3efca53f282252a
Tahmina Akter <mrmirofficial@gmail.com>
2021-11-26 20:35:31 +0600

    admin defaulefault

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Main from './AdminInc/Main';
+import Nav from './AdminInc/Nav';
+import Sidebar from './AdminInc/Sidebar';
 
 export default function AdminLayout() {
+    useEffect(() => {
+        document.body.classList.add("hold-transition");
+        document.body.classList.add("sidebar-mini");
+    }, [])
     return (
         <>
-            admin layout
+            <div class="wrapper">
+                <Nav />
+                <Sidebar />
+                <Main />
+
+                <aside class="control-sidebar control-sidebar-dark">
+                </aside>
+            </div>
         </>
     )
 }
